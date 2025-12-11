107e794cb04b270829340873812eccae5bdb0adf
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-11 17:29:35 +0530

    Refactor rearrange array implementation; fix docstring typo and add new logic for handling unequal positives and negatives.

--- Arrays/Rearrange Positives and Negatives in a Array.py
+++ Arrays/Rearrange Positives and Negatives in a Array.py
@@ -18,7 +18,7 @@ class Solution(object):
 
         return ans
 
-TC-> O(n) -> Juts Iterating through the Loop 
+TC-> O(n) -> Just Iterating through the Loop 
 SC -> O(n) -> For Storing the answer Array 
 
 Time Complexity : O(n^2) -> Not Optimal but constant space
@@ -44,4 +44,58 @@ while(i < len(L)):
             
     i += 1
 print(L)
-        
+
+""" 
+
+Another Pattern is what if the number of positives and negatives are not same 
+
+Time Complexity : O(n) -> for finding the number of +ve and -ves + O(n) for putting them at the correct positions 
+                 Tota; -> O(2n) -> O(n)
+Space Complexity : O(n) -> For additional ans array
+
+"""
+L = eval(input("Enter the List : "))
+
+pos = []
+neg = []
+
+for x in L:
+    if x > 0:
+        pos.append(x)
+    else:
+        neg.append(x)
+
+ans = [0] * len(L)
+
+i = 0   
+j = 1   
+
+
+p = 0
+n = 0
+
+while p < len(pos) and n < len(neg):
+    ans[i] = pos[p]
+    ans[j] = neg[n]
+    i += 2
+    j += 2
+    p += 1
+    n += 1
+
+while p < len(pos):
+    if i >= len(L):
+        i = j 
+    ans[i] = pos[p]
+    i += 1
+    p += 1
+
+while n < len(neg):
+    if j >= len(L):
+        j = i  
+    ans[j] = neg[n]
+    j += 1
+    n += 1
+
+print(ans)
+
+    
