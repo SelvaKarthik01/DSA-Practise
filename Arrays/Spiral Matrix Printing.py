72cb526131471f52d8a16831b81cb3aebdb1bc9d
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-12 12:03:48 +0530

    Fix print statements in spiral matrix printing to include end parameter for proper formatting

--- Arrays/Spiral Matrix Printing.py
+++ Arrays/Spiral Matrix Printing.py
@@ -15,18 +15,18 @@ bottom = rows-1
 right = cols-1
 while(top <= bottom and left <= right):
     for i in range(left,right+1):
-        print(matrix[top][i])
+        print(matrix[top][i],end=" ")
     top += 1
     for i in range(top,bottom+1):
-        print(matrix[i][right])
+        print(matrix[i][right],end = " ")
     right -= 1
     if top <= bottom:
         for i in range(right,left-1,-1):
-            print(matrix[bottom][i])
+            print(matrix[bottom][i],end = " ")
         bottom-=1
     if left <= right:
         for i in range(bottom,top-1,-1):
-            print(matrix[i][left])
+            print(matrix[i][left],end=" ")
         left += 1
 
             
