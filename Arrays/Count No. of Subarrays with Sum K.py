574999ce67f36db7aafe1d58e7fd5abe8aa0aed8
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-12 12:19:10 +0530

    Update docstring for Count No. of Subarrays with Sum K to enhance clarity and detail

--- Arrays/Count No. of Subarrays with Sum K.py
+++ Arrays/Count No. of Subarrays with Sum K.py
@@ -1,3 +1,15 @@
+"""
+Docstring for Arrays.Count No. of Subarrays with Sum K
+
+Another Appraoch is to create all possible subarrays and find those subarrays whose element sum is k 
+TC-> O(n^2) -> n^2 for subarray with element i
+SC -> O(1) 
+
+
+Time Complexity : O(n)
+Space Complexity : O(n) for the seen hashmap 
+
+"""
 L = eval(input("Enter the list : "))
 k = int(input("Enter the Sum : "))
 d = {}
