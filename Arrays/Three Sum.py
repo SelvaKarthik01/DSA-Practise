72ce193d6522b6f839720a5612470747583ece76
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-12 22:42:14 +0530

    Refactor Three Sum implementation for clarity and efficiency; update docstring for better understanding

--- Arrays/Three Sum.py
+++ Arrays/Three Sum.py
@@ -1,26 +1,50 @@
+"""
+Docstring for Arrays.Three Sum
+
+Another Approarch is to find the all possible triplets and find the triplets with sum as target 
+
+TC-> O(N^3) 
+SC -> O(Number of triplets)
+
+ANother Approach is using the fact that 
+arr[i] + arr[j] + arr[k] = target 
+arr[k] = target - arr[i] - arr[j]
+
+We only loop thourgh i and j and find the target-arr[i]-arr[j] present in hashmap 
+TC-> O(n^2) only for i and j 
+SC -> O(n)
+
+Time Complexity: O(nlogn) for sorting + O(n^2) for keeping i constant and moving j and k 
+                 Total -> O(nlogn + n^2) -> O(n^2)
+Space Compleixty : O(1) -> Auxiliary Space O(Number of triplets)
+
+This is very similar to the Two Sum but here we are keeping i as constant and perfroming the same 
+"""
 L = eval(input("Enter the List : "))
-target = int(input("Enter the Target Sum : "))
 L.sort()
+target = int(input("Enter the Target Element : "))
+i = 0 
 ans = []
-i = 0
 while(i < len(L)):
-    prev = L[i]
     j = i +1 
     k = len(L)-1
-    while(j < k):
-        sum = L[i]+L[j]+L[k]
-        if sum == target :
-            ans.append([L[i],L[j],L[k]])
-            temp = L[j]
-            while(j < k and L[j] == temp):
-                j += 1
-            temp = L[k]
-            while(j < k and L[k] == temp):
-                k -= 1
-        elif sum > target:
-            k -= 1
-        elif sum < target :
+    while(k >= 0 and j < len(L) and j < k):
+        if L[i] + L[j] + L[k] < target:
             j += 1
-    while(i < len(L) and L[i] == prev):
+        elif L[i] + L[j] + L[k] > target:
+            k -= 1 
+        else:
+            if L[i] + L[j] + L[k] == target:
+                ans.append([L[i],L[j],L[k]])
+                temp = L[j]
+                while(j < len(L) and L[j] == temp):
+                    j += 1
+                temp = L[k]
+                while(k >= 0 and L[k] == temp):
+                    k -= 1
+    temp = L[i]
+    while(i < len(L) and L[i]==temp):
         i += 1
 print(ans)
+                
+            
