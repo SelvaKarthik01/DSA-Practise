41d6b640cb3447968663da9e9a0e4271c70b4a30
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-14 08:57:19 +0530

    Implement Maximum Product Subarray algorithm; add initial code and docstring for clarity

---
+++ Arrays/Maximum Product Subarray.py
@@ -0,0 +1,27 @@
+"""
+Docstring for Arrays.Maximum Product Subarray
+
+Another Approach is to generate all the subarrays anf ind the rpoducts and cehck if its max
+TC-> O(n^2)
+SC -> O(1)
+
+Another Appraoch a Simulation of Kadanes Algorithm which is not intuitive
+Same and Time and Space and Complexity as we this Algorithm 
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+
+"""
+L = eval(input("Enter the List : "))
+maxi = float("-inf")
+prefix = 1 
+suffix = 1 
+for i in range(len(L)):
+    if prefix == 0:
+        prefix = 1
+    elif suffix == 0:
+        suffix = 1
+    prefix *= L[i]
+    suffix *= L[(len(L)-1)-i]
+    maxi= max(maxi,max(prefix,suffix))
+print(maxi)
