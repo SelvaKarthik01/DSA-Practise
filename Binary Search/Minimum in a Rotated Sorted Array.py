9aaffd8e01d7c03b5d4d383ba76ac4b76c347c29
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-14 18:07:46 +0530

    Implement Minimum in a Rotated Sorted Array algorithm; add functionality and docstring for clarity

---
+++ Binary Search/Minimum in a Rotated Sorted Array.py
@@ -0,0 +1,28 @@
+"""
+Docstring for Binary Search.Minimum in a Rotated Sorted Array
+
+Time Complexity : O(logn)
+Space Compleixty : O(1)
+
+"""
+
+def Binary_Search(L):
+    ans = float("inf")
+    low = 0 
+    high = len(L)-1
+    while(low <= high):
+        mid = low + (high-low)//2
+        if L[low] == L[mid] == L[high]:
+            low += 1
+            high -= 1
+            continue 
+        elif L[low] <= L[mid]:
+            ans = min(ans,L[low])
+            low = mid + 1
+        else:
+            ans = min(ans,L[mid])
+            high = mid - 1 
+    return ans  
+L = eval(input("Enter the List : "))
+print(Binary_Search(L,))
+
