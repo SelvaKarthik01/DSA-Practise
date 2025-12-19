ca29f60040fa234c71ec64d52c967d44b7696b88
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-19 14:10:24 +0530

    Implement Insert at kth Position in Linked List with display and length functions

---
+++ Linked Lists/Insert at kth Position in the Linked List.py
@@ -0,0 +1,61 @@
+"""
+Docstring for Linked Lists.Insert at kth Position in the Linked List
+
+Time Complexity : O(k) or O(1) at k = 1
+Space Complexity : O(1)
+
+"""
+
+class Node:
+    def __init__(self,val,next=None):
+        self.val = val
+        self.next = next 
+    def __str__(self):
+        return f"{self.val}"
+
+def Insert(head,val):
+    newnode = Node(val)
+    if head == None:
+        return newnode 
+    else:
+        curr = head
+        while(curr.next != None):  # Until we reach the Last Node in Linked List  
+            curr = curr.next 
+        curr.next = newnode 
+    return head  
+def display(head):
+    print(head.val,end="") 
+    curr= head.next
+    while(curr):
+        print("->",curr.val,end = "")
+        curr = curr.next 
+def Length(head):
+    count = 0 
+    curr = head 
+    while(curr):
+        curr =curr.next
+        count += 1
+    return count
+def InsertAtk(head,val,k):
+    newnode = Node(val)
+    if k == 1:
+        newnode.next = head
+        return  newnode 
+    else:
+        curr= head 
+        count = 1
+        while(count !=k-1 and curr.next != None):
+            curr = curr.next 
+            count += 1
+        newnode.next = curr.next 
+        curr.next = newnode 
+    return head 
+         
+L = [1,2,3,4,5,6,7,8,9,10]
+head = None
+for i in range(len(L)):
+    head = Insert(head,L[i])
+k = int(input("Enter the Value for K : "))
+val = int(input("Enter the value to be Inserted : "))
+head = InsertAtk(head,val,k)
+display(head)
