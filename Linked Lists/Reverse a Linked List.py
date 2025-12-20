04c3f1a483be559234cc5cc366668d40f09b8886
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-20 14:25:17 +0530

    Refactor reverse linked list implementation and update helper functions for clarity and consistency

--- Linked Lists/Reverse a Linked List.py
+++ Linked Lists/Reverse a Linked List.py
@@ -1,39 +1,70 @@
+"""
+Docstring for Linked Lists.Reverse a Linked List
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+
+Another Solution with just Two Pointers Only Possible in Python
+
+class Solution(object):
+    def reverseList(self, head):
+        if head is None or head.next is None:
+            return head 
+        prev = None 
+        curr = head 
+        while(curr.next != None):
+            prev,curr.next,curr = curr,prev,curr.next
+            
+        curr.next = prev 
+        return cur
+        
+Same TC and SC 
+
+"""
 class Node:
     def __init__(self,val,next=None):
         self.val = val
-        self.next = next
+        self.next = next 
     def __str__(self):
         return f"{self.val}"
-def insert(root,val):
+
+def Insert(head,val):
     newnode = Node(val)
-    if root == None:
-        return newnode
-    curr = root
-    while(curr.next is not None):
-        curr = curr.next 
-    curr.next = newnode
-    return root
-def display(root):
-    curr = root
-    while(curr is not None):
-        print(curr,end=" -> ")
-        curr = curr.next
-    print("null")
-    
-def reverse(root):
-    if root.next is None:
-        return root
+    if head == None:
+        return newnode 
     else:
-        prev = None
-        head = root
-        while head:
-            head.next,prev,head = prev,head,head.next 
-    return prev
-            
-L = eval(input("Enter the List of Nodes : "))
-root = None
+        curr = head
+        while(curr.next != None):  # Until we reach the Last Node in Linked List  
+            curr = curr.next 
+        curr.next = newnode 
+    return head 
+ 
+def display(head):
+    print(head.val,end="") 
+    curr= head.next
+    while(curr):
+        print("->",curr.val,end = "")
+        curr = curr.next 
+
+def ReverseLL(head):
+    if head is None or head.next is None:
+        return head 
+    prev = None 
+    curr = head 
+    future = head.next 
+    while(curr.next != None):
+        curr.next = prev 
+        prev = curr 
+        curr = future 
+        future = future.next 
+    curr.next = prev 
+    return curr 
+    
+    
+        
+L = [1,2,3,4,5,6,7,8,9,10]
+head = None
 for i in range(len(L)):
-    root = insert(root,L[i])
-display(root)
-root = reverse(root)
-display(root)
+    head = Insert(head,L[i])
+head = ReverseLL(head)
+display(head)
