c29ce97bf052c1c9f6fcd0df392465412e88649c
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2025-12-22 21:54:13 +0530

    Add implementation for a browser history using a doubly linked list

---
+++ Linked Lists/Design a Browser History.py
@@ -0,0 +1,51 @@
+"""
+Docstring for Linked Lists.Design a Browser History
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+
+"""
+
+class Node:
+    def __init__(self,val,next=None,prev=None):
+        self.val = val 
+        self.next = next 
+        self.prev = prev 
+    def __str__(self):
+        return f"{self.val}"
+
+class BrowserHistory:
+    def __init__(self,homepage):
+        self.curr = Node(homepage)
+    
+    def visit(self,url):
+        newnode = Node(url)
+        newnode.prev = self.curr 
+        self.curr.next = newnode 
+        self.curr = newnode 
+        return self.curr.val
+    
+    def back(self,steps):
+        while(self.curr.prev != None and steps):
+            self.curr = self.curr.prev 
+            steps -= 1
+        return self.curr.val 
+    
+    def forward(self,steps):
+        while(self.curr.next != None and steps):
+            self.curr = self.curr.next 
+            steps -= 1
+        return self.curr.val 
+
+Browser = BrowserHistory("Google.com")
+print(Browser.visit("Leetcode.com"))
+print(Browser.visit("Instagram.com"))
+print(Browser.visit("Facebook.com"))
+print(Browser.back(1))
+print(Browser.back(1))
+print(Browser.forward(1))
+print(Browser.visit("Leetcode.com"))
+print(Browser.forward(2))
+print(Browser.back(2))
+print(Browser.back(7))
+
