ad947cbad21f60f1efa714afc9793614d2f2bf45
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2026-02-15 09:26:57 +0530

    Refactor SPF implementation with updated docstring and time complexity analysis

--- Numbers/SPF Smallest Prime Factorization.py
+++ Numbers/SPF Smallest Prime Factorization.py
@@ -1,17 +1,29 @@
-n = int(input("Enter the Number : "))
-seive = [0]*(n+1)
-def soe(seive,n):
-    for i in range(2,n+1):
-        a = 1
-        while(i*a <= n):
-            if seive[i*a] == 0:
-                seive[i*a] = i
-            a += 1
-    
-ans = []
-soe(seive,n)
-while(n!=1):
-    ans.append(seive[n])
-    n = n // seive[n]
-print(ans)
-print("BADHUSHAH")
+"""
+Docstring for Numbers.SPF Smallest Prime Factorization
+
+Time Complexity : O(n) -> Creating the Sieve Array + O(nlog(logn)) -> Prime Harmonic Series + O(k) -> k Smallest Prime Numbers 
+                 => O(mlog(logn))
+Space Complexity : O(n) -> Sieve Array Space 
+"""
+import math 
+def soe(n):
+    sieve = [1]*(n+1) # O(n) 
+    sieve[0]=sieve[1] = 0 
+    for i in range(2,int(math.sqrt(n)+1)): # O(nlog(logn))
+        if sieve[i] == 1:
+            mul = i 
+            while(i*mul <= n):
+                if sieve[i*mul] == 1:
+                    sieve[i*mul] = i 
+                mul += 1
+    return sieve 
+
+n = int(input("Enter the Number N : "))
+prime_factors = []
+sieve = soe(n)
+print(f"Smallest Prime Factors of {n} : ",end = " ")
+while(sieve[n] != 1):         # O(k)    
+    prime_factors.append(sieve[n])
+    n = n // sieve[n]
+prime_factors.append(n)
+print(prime_factors)
