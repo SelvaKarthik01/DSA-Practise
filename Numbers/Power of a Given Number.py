a3a32415e78775d2dd5117782393ab1c3e79ea4b
SelvaKarthik01 <selvakarthikfanofvk2004@gmail.com>
2026-02-15 08:24:07 +0530

    Refactor power calculation and prime factors implementation with updated docstrings and complexity details

--- Numbers/Power of a Given Number.py
+++ Numbers/Power of a Given Number.py
@@ -1,18 +1,28 @@
-a = int(input("Enter the Number for a : "))
-b = int(input("Enter the Number for b : "))
+"""
+Docstring for Numbers.Power of a Given Number
+
+Time Complexity : O(logn)
+Space Complexity : O(1)3
+"""
+
+a = eval(input("Enter the Base Number : "))
+b = int(input("Enter the Power Raised to : "))
+
 flag = 0
-if b < 0:
-    b = b * -1
-    flag = 1
-ans = 1
-while(b!=0):
-    if b & 1 == 1:
-        ans *= a 
-        b -= 1
+if b < 0 :
+    flag = 1 
+    b = b * -1 
+ans = 1 
+while(b != 0):
+    if b % 2 == 0:
+        a = a * a 
+        b = b//2       # O(logn)
     else:
-        a = a ** 2
-        b = b //2
-if flag == 1:
+        ans = ans * a 
+        b -= 1
+
+if flag:
     print(1/ans)
 else:
     print(ans)
+    
--- Numbers/Print all Prime Factors of a Number.py
+++ Numbers/Print all Prime Factors of a Number.py
@@ -1,22 +1,20 @@
 """
 Docstring for Numbers.Print all Prime Factors of a Number
 
-Time Complexity : O(sqrt(n))
-Space Complexity : O(n) Auxiliary Space 
-
+Time Complexity : O(sqrt(n)*logn)
+Space Complexity : O(logn) # The PRoduct first n prime numbers grows very fast 
 """
-import math 
-def PrimeFactors(n):
-    L = [1]
-    for i in range(2,int(math.sqrt(n)+1)):
+import math
+def prime_factors(n):
+    L = []
+    for i in range(2,int(math.sqrt(n)+1)): # O(sqrt(n))
         if n % i == 0:
             L.append(i)
-            while(n%i == 0):
+            while(n%i==0): # O(logn)
                 n = n // i 
     if n != 1:
         L.append(n)
-    return L
-    
-n = int(input("Enter the Number N : "))
+    return L 
 
-print(PrimeFactors(n))
+n = int(input("Enter the Value for N : "))
+print(prime_factors(n))
